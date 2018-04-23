d386e6dcbfb3c84f9d6e623ced2664b14ce6b80c
matthi <mruhland01@gmail.com>
2018-04-23 13:26:59 -0700

    hooked up socket event handlers for connect and disconnect

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 node chat app - using socket io
+
+hosted on heroku at https://intense-wave-25014.herokuapp.com/ 
--- server/server.js
+++ server/server.js
@@ -1,15 +1,29 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 const app = express();
+// http and createServer is already used in the background with express, but need to make some changes to integrate socket io
+const server = http.createServer(app);
+const io = socketIO(server); //returns web socket server
 
 //express static middleware to server up the public folder
 app.use(express.static(publicPath));
 
+//io.on() lets you initialize an event listener
+io.on('connection', (socket) => {
+    console.log('New user connected');
 
-app.listen(port, () => {
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`Server up on port ${port}`);
 });
